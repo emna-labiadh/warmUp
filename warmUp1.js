1460cb221221fee09205415c5ffceb7eee66e1e7
emna-labiadh <emnalolita@gmail.com>
2020-03-13 13:49:03 +0100

    i finish

--- warmUp1.js
+++ warmUp1.js
@@ -1,6 +1,18 @@
 // 1-using + operator combine your partner first and last name .
+   
+   function name(first,last) {
+   	return first + last
+   }
 // 2-find if the number 13 is a multiple of 3 or not.
-// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
-// 4-calculate your age in seconds.
+function multiple(x,y){
+	var z= x%y;
+	if (z===0){
+		return x +' '+ 'is a multiple of'+' '+ y;
+	}
+	return x +' '+'is not a multiple of'+' '+ y;
+} 
 
-// your code is here
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+// 4-calculate your age in secon
+// your code  here
+
