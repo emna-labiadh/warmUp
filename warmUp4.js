90046a01f0c3fe81a6b9686d6311f4920a6de9a7
emna-labiadh <emnalolita@gmail.com>
2020-03-17 21:32:55 +0100

    done

--- warmUp4.js
+++ warmUp4.js
@@ -1,6 +1,15 @@
 // 1-write a function that returns the product of any number by 3.
+function times3(x) {
+	return x*3
+}	
+	
+
 
 // 2-complete the following function multiply to return the result of multiplying of x and y
+function multiplay(x,y){
+	return x*y;
+}
+	
 
 // 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is
 // 	(5/9) * (fahrenheit-32);
