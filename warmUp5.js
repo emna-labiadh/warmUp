bda4005477ab5f2222fae7573c2adf26f94a1a2e
emna-labiadh <emnalolita@gmail.com>
2020-03-18 23:08:47 +0100

    done

--- warmUp5.js
+++ warmUp5.js
@@ -2,7 +2,9 @@
 // - takes 2 arguments, both numbers.
 // - returns whichever number is the greater (higher) number.
 // ex greaterNum(5, 10) => "The greater number of 5 and 10 is 10."
-
+function greaterNum(a,b) {
+	return a < b
+}
 // 2) Write a function named isEven using  for loop that
 // -  iterate from x to y.
 // -  return array contain the even values,
@@ -14,13 +16,29 @@
 //3) write a function named sum that
 // - Use a while loop to add up the numbers from x to y.
 // ex sum(1,5) => 15
-
+function sum(n) {
+ var counter = 1
+ var total   = 0 
+ while(counter <= n)
+ total = total + counter
+ counter = counter + 1
+}
+return total
+}
 //4) Write a function named factorial that
 // -  Use Recursion to calculate the factorial of a number
 // -  the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n
 // - 5! = 5*4*3*2*1 = 120
 // ex : factorial(5) => 120
-
+function factorial(n) {
+	var counter = 1
+	var total   = 1
+	while (counter <= n) {
+		total = total * counter 
+		counter = counter + 1
+	}
+    return total 
+}
 //5) write a function named decimals
 //- the function will format a number up to specified decimal places
 //- the function will return a string
