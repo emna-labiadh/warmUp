9d33519ba07eea4fa629d00ca863f084f0b0da51
emna-labiadh <emnalolita@gmail.com>
2020-03-20 17:09:48 +0100

    my work

--- warmUp7.js
+++ warmUp7.js
@@ -1,12 +1,29 @@
 // 1-Choose the correct comparison operator to display "true", when: 5 is less than 7.
 //answer :
+function operator(a , b) {
+	if (a < b) {
+		return true 
+	}
+     return false
+}
+
+
+
 
 //2- Write a function named helloWorld that:
 //takes 1 argument, a language code (e.g. "fr", "es", "en")
 //returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
 //ex: helloWorld('fr') ==> 'Bonjour tout le monde'
 //    helloWorld('es') ==> 'Hola, Mundo'
-//    helloWorld('') ==> 'Hello, World'
-
+//  
+function helloWorld(fr, es, en) {
+    if (fr == Bounjour tout le monde) {
+   	else if (es == Hola Mondo)
+   } 
+   else ('' == Hollo world)
+  }
+{
+	return ''
+}
 //3- write a function to Reverse an array without use array.reverse() method:
 // ex: reverseArray([1,2,3]) ==> [3,2,1]
