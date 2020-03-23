5dd3227c5b2bf00abb72e0efaed6d585354ae81f
emna-labiadh <emnalolita@gmail.com>
2020-03-23 10:36:00 +0100

    hard

--- warmUp9.js
+++ warmUp9.js
@@ -4,7 +4,15 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
-
+    function Array([input]) {
+       if([1,2].isArray) {
+         return true 
+     }
+      if else ((5).isArray) {
+         return false
+  }
+}
+    
 // **************** git status ****************
 // **************** git add fileName.js ****************
 // **************** git status ****************
@@ -16,6 +24,10 @@
 //ex  first([1,2,3],1)=>[1]
 //    first([1,2,3,4]2)=>[1,2]
 
+  function ([n]) {
+
+  }
+
 // **************** git status ****************
 // **************** git add fileName.js ****************
 // **************** git status ****************
@@ -25,7 +37,12 @@
 
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+     const object = {
+     	name: 'ahmed',
+     	age: 20
+     };
 
+     	     
 // **************** git status ****************
 // **************** git add fileName.js ****************
 // **************** git status ****************
